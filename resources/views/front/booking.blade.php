8a91f883d6c0fb8a72de2ab1991073c9cd90704e
Naufal <naufalilham2104@gmail.com>
2026-03-12 13:14:51 +0700

    Update tampilan booking

--- resources/views/front/booking.blade.php
+++ resources/views/front/booking.blade.php
@@ -114,12 +114,13 @@
                     <div class="bg-white rounded-3xl shadow-sm border border-gray-100 p-8 min-h-[450px] relative">
 
                         <div
-                            class="flex flex-col sm:flex-row justify-between sm:justify-center items-start sm:items-center mb-8 border-b border-gray-100 pb-4 gap-3 sm:gap-0">
-                            <div>
+                            class="flex flex-col sm:flex-row justify-between items-center mb-8 border-b border-gray-100 pb-4 gap-3 sm:gap-0">
+                            <div class="text-center sm:text-left">
                                 <h3 class="text-md lg:text-xl font-bold text-gray-800">Pilih Jam Foto</h3>
                                 <p class="text-sm text-gray-400 mt-1">Sesi foto 11:00 - 18:00 WIB</p>
                             </div>
-                            <div x-show="formattedDate" class="w-full sm:w-auto text-left sm:text-right mt-1 sm:mt-0">
+
+                            <div x-show="formattedDate" class="w-full sm:w-auto text-center sm:text-right mt-1 sm:mt-0">
                                 <span x-text="formattedDate"
                                     class="text-xs lg:text-sm font-bold text-red-600 bg-red-50 px-4 py-2 rounded-lg md:rounded-xl border border-red-100 inline-block"></span>
                             </div>
