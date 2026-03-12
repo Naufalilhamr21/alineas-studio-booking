940723578720fd01dce771036deeead781f048fe
Naufal <naufalilham2104@gmail.com>
2026-03-12 12:22:37 +0700

    Migrasi WebSocket dari Pusher ke Laravel Reverb

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,17 +3,16 @@ import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    // broadcaster: "reverb",
-    // key: 'kds15c8yiaqzrzauk1o7',
-    // wsHost: '127.0.0.1',
-    // wsPort: 8081,
-    // // INI YANG PALING PENTING:
-    // forceTLS: false, // Paksa matikan TLS/HTTPS
-    // disableStats: true,
-    // enabledTransports: ["ws"], // Paksa hanya gunakan 'ws://', abaikan 'wss://'
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 
-    broadcaster: "pusher",
-    key: 'bdad83b91853b465a3eb',
-    cluster: 'ap1',
-    forceTLS: true, // Ini yang membuat koneksinya 100% aman (WSS) melewati Ngrok!
+    // broadcaster: "pusher",
+    // key: 'bdad83b91853b465a3eb',
+    // cluster: 'ap1',
+    // forceTLS: true,
 });
