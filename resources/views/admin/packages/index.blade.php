c87c42b4a6d23432b67e2b5c8d07b9818cebbdf1
Naufal <naufalilham2104@gmail.com>
2026-05-11 07:52:57 +0700

    Fix notifikasi sukses

--- resources/views/admin/packages/index.blade.php
+++ resources/views/admin/packages/index.blade.php
@@ -27,7 +27,7 @@
 
             @if (session('success'))
                 <div
-                    class="bg-green-50 border-l-4 border-green-500 text-green-700 p-4 mx-6 mt-6 rounded-r-lg flex items-center gap-2">
+                    class="bg-green-50 border-l-4 border-green-500 text-green-700 p-4 py-3 rounded-r-lg flex items-center gap-2">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
                         </path>
